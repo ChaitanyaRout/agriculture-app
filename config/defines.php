ff4bc04d80092e5ff4ace90c35dc652f97d817d9
Chaitanya Rout <chaitanyarout1997@gmail.com>
2021-03-21 18:30:15 +0530

    included files

---
+++ config/defines.php
@@ -0,0 +1,25 @@
+<?php
+	require_once "smarty/libs/smarty.class.php";
+
+	require_once 'vendor/autoload.php';
+
+	require_once 'server_config.php';
+	require_once 'db_config.php';
+	require_once 'classes/db/Db.php';
+
+	require_once 'classes/orm/States.php';
+	require_once 'classes/model/ModelStates.php';
+
+	require_once 'classes/orm/District.php';
+	require_once 'classes/model/ModelDistrict.php';
+
+	require_once 'classes/orm/Scheme.php';
+	require_once 'classes/model/ModelScheme.php';
+
+	require_once 'classes/orm/Crop.php';
+	require_once 'classes/model/ModelCrop.php';
+
+	require_once 'classes/helper/Helper.php';
+	
+
+?>
